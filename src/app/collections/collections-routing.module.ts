1ed2a944ac301abe2fbc78ff285c7aa813c850e0
iamharry <harender_24@hotmail.com>
2020-02-09 23:23:14 +0530

    code updated

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CollectionsHoneComponent } from './collections-hone/collections-hone.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnerComponent } from './partner/partner.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: CollectionsHoneComponent
+    component: CollectionsHoneComponent,
+    children: [
+      { path: '', component: BiographyComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'partners', component: PartnerComponent }
+    ]
   }
 ];
 
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHoneComponent } from './collections-hone/collections-hone.component';
-
+import { TableComponent } from './table/table.component';
+import { SharedModule } from '../shared/shared.module';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnerComponent } from './partner/partner.component';
 
 @NgModule({
-  declarations: [CollectionsHoneComponent],
+  declarations: [CollectionsHoneComponent, TableComponent, BiographyComponent, CompaniesComponent, PartnerComponent],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule, SharedModule
   ],
   exports: []
 })
