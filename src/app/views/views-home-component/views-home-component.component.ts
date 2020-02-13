d1f040221c2c08f6ba7efa57bb00bd2635ead2e1
iamharry <harender_24@hotmail.com>
2020-02-14 01:02:20 +0530

    Merged and Updated

--- src/app/views/views-home-component/views-home-component.component.ts
+++ src/app/views/views-home-component/views-home-component.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./views-home-component.component.css']
 })
 export class ViewsHomeComponentComponent implements OnInit {
-
+  stats = [
+    { value: 22, label: '# of Users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Reviews' }
+  ];
+  items = [
+    {
+      image: '/assets/images/couch.jpeg',
+      title: 'Couch',
+      description: 'This is a fantastic couch to sit on'
+    },
+    {
+      image: '/assets/images/dresser.jpeg',
+      title: 'Dresser',
+      description: 'This is a great dresser to purchase'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponentComponent } from './views-home-component/views-home-component.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { SharedModule } from '../shared/shared.module';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
-  declarations: [ViewsHomeComponentComponent, StatisticsComponent],
+  declarations: [ViewsHomeComponentComponent, StatisticsComponent, ItemListComponent],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ]
 })
 export class ViewsModule { }
